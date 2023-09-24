b6b37e257bb69bb3d801aeabec562c2ccf0b521a
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-09-25 00:31:12 +0500

    added state to the Form.jsx and now modifying the state

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react'
 
-export default function Form({ handleSubmit }) {
+const initialInputs = {
+    'current-savings': 1000,
+    'yearly-contribution': 1200,
+    'expected-return': 7,
+    'duration': 10
+}
 
+export default function Form() {
+
+    const [userInput, setUserInput] = useState(initialInputs)
 
     const handlingSubmit = (evnt) => {
         evnt.preventDefault();
-        console.log('Submitted')
+
     }
 
     const resetHanlder = () => {
-        console.log('Restted')
+
     }
 
     const inputChangeHandler = (input, value) => {
-        console.log(input, value)
+        setUserInput(prevInput => {
+            return {
+                ...prevInput,
+                [input]: value
+            }
+        })
     }
 
 
@@ -26,13 +39,17 @@ export default function Form({ handleSubmit }) {
                     <label htmlFor="current-savings">Current Savings ($)</label>
                     <input onChange={(evnt) => {
                         inputChangeHandler('current-savings', evnt.target.value)
-                    }} type="text" id="current-savings" />
+                    }} type="text" id="current-savings"
+                        value={userInput['current-savings']}
+                    />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
                     <input onChange={(evnt) => {
                         inputChangeHandler('yearly-contribution', evnt.target.value)
-                    }} type="number" id="yearly-contribution" />
+                    }} type="number" id="yearly-contribution"
+                        value={userInput['yearly-contribution']}
+                    />
                 </p>
             </div>
             <div className="input-group">
@@ -42,13 +59,17 @@ export default function Form({ handleSubmit }) {
                     </label>
                     <input onChange={(evnt) => {
                         inputChangeHandler('expected-return', evnt.target.value)
-                    }} type="number" id="expected-return" />
+                    }} type="number" id="expected-return"
+                        value={userInput['expected-return']}
+                    />
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
                     <input onChange={(evnt) => {
                         inputChangeHandler('duration', evnt.target.value)
-                    }} type="number" id="duration" />
+                    }} type="number" id="duration"
+                        value={userInput['duration']}
+                    />
                 </p>
             </div>
             <p className="actions">
